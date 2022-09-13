7e0f7c31f03d5764254243afa37fa5f99c29457a
CyrilAmbwaya <acyril14@gmail.com>
2022-09-13 08:23:30 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-#include "main.h"
+#include <main.h>
 /**
   * main - Entry point
   *
-  * Return: Always 0
+  * Return: Always 0 (Success)
   */
 int main(void)
 {
