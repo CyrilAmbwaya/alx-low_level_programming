4e3f4bfc9071c1452708f9a45529a9bdef8cb030
CyrilAmbwaya <acyril14@gmail.com>
2022-11-02 12:24:06 -0700

    fileIO

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,28 @@
-/**
-*read_textfile - function that reads a text file and prints it to stdout
-*@filename : name of file to be read
-*@letters : number of characters to be read
-*Return: number of bytes read
-*/
 #include "main.h"
+
+/**
+ * read_textfile - reads and prints content of a file
+ * @filename: file name
+ * @letters: number of letters to print
+ *
+ * Return: Number of chars printed or 0
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-char *buffer;
-size_t wr; /*byte read and written*/
+	int fd, retval, tmp;
+	char *buff;
 
-if (!filename)
-return (0);
-
-fd = open(filename, O_RDONLY, 0444);
-
-if (fd < 0)
-return (0);
-
-buffer = malloc(sizeof(char) * letters);
-wr = read(fd, buffer, letters);
-write(1, buffer, wr);
-
-close(fd);
-
-return (wr);
-
-
-
+	if (!filename)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	if (!buff)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	tmp = read(fd, buff, letters);
+	retval = write(1, buff, tmp);
+	free(buff);
+	if (fd == -1 || tmp == -1 || retval == -1 || retval != tmp)
+		return (0);
+	close(fd);
+	return (retval);
 }
