50b1db273a638b740907f129b563b0a0fcdb817d
CyrilAmbwaya <acyril14@gmail.com>
2022-11-02 12:35:50 -0700

    fileIO

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,39 +1,31 @@
 #include "main.h"
 
 /**
-*create_file - function to create and write into a file
-*@filename: file to create
-*@text_content: content to be written into file
-*Return: 1 on success or -1 on failure
-*/
-
+ * create_file - creates a file and add content to it
+ * @filename: file name
+ * @text_content: content to put in the file
+ *
+ * Return: 1 for success or -1 if it fails
+ */
 int create_file(const char *filename, char *text_content)
 {
-int fd, i;
-size_t len = 1;
+	int fd, i = 0, retval = 1;
 
-if (!filename)
-return (-1);
-
-fd = open(filename, O_RDONLY | O_TRUNC);
-
-if (fd < 1)
-{
-fd = open(filename, O_CREAT, 0600);
-
-if (fd < 1)
-return (-1);
-
-}
-
-
-if (!text_content)
-return (1);
-
-for (i = 0; text_content[i] != '\0'; len++, i++)
-;
-
-write(fd, text_content, len);
-return (1);
-
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+	while (text_content[i])
+		i++;
+	retval = write(fd, text_content, i);
+	close(fd);
+	if (retval == -1 || retval != i)
+		return (-1);
+	return (1);
 }
