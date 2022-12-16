67b5dd03314a037c969d3bb8af5cec80917cca92
CyrilAmbwaya <acyril14@gmail.com>
2022-12-16 13:35:05 -0800

    Dynamic libs

--- 0x18-dynamic_libraries/0-isupper.c
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -1,15 +1,20 @@
 #include "main.h"
+
 /**
-* _isupper - function
-*
-* @c: parameter
-*
-* Return: return value
-*/
+ *_isupper - a function that checks for uppercase characters
+ *@c: a variable that stores letters
+ *Return: Returns a value of 1 if the variable c contains an uppercase
+ *letter.
+ */
+
 int _isupper(int c)
 {
-if (c >= 65 && c <= 90)
-return (1);
-else
-return (0);
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
--- 0x18-dynamic_libraries/0-memset.c
+++ 0x18-dynamic_libraries/0-memset.c
@@ -1,20 +1,20 @@
 #include "main.h"
+
 /**
-* _memset - fills memory with a constant byte
-*
-* @n: buffer size
-* @s: bytes of the memory area
-* @b: constant byte
-*
-* Return: a pointer
-*/
+ * _memset - Files memory with a constant byte
+ * @s: buffer array
+ * @b: constant byte
+ * @n: number of bytes of memory area to fill
+ * Return: Pointer to memory area *s
+ */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int x;
+	unsigned int i;
 
-	for (x = 0; x < n; x++)
+	for (i = 0; i < n; i++)
 	{
-		*(s + x) = b;
+		s[i] = b;
 	}
 	return (s);
 }
